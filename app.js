42668fc2b45704633f1c80a01b8686f5b6004409
Ahmed Zubair <153666614+AhmedZubair18@users.noreply.github.com>
2024-08-03 10:53:15 +0500

    Elevator

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
-function moveLift(floor) {
-    const lift = document.getElementById('lift');
-    const floors = {
-        third: 450,
-        second: 300,
-        first: 150,
-        ground: 0
-    };
-    lift.style.bottom = floors[floor] + 'px';
+function moveElevator(position) {
+    const elevator = document.getElementById('elevator');
+    elevator.style.bottom = position + 'px';
 }
